66befd304d4709b12fe43f17baf765c66c857b45
iillya <liuwenbodnss@gmail.com>
2026-08-14 00:26:09 +0800

    开关提示改用自定义文案覆盖默认（空格方案无效）

--- source/NoLeftClickRotationPython.py
+++ source/NoLeftClickRotationPython.py
@@ -631,12 +631,12 @@ def _setup_ui():
     zbc.add_subpalette(PALETTE, title_mode=0)
     zbc.add_switch(
         ENABLE_PATH, True,
-        " ",
+        "启用或禁用插件",
         _toggle, initially_disabled=False, width=150,
     )
     zbc.add_switch(
         CAM_LOCK_PATH, True,
-        " ",
+        "控制相机锁定与右键旋转",
         _toggle_cam_lock, initially_disabled=False, width=150,
     )
     zbc.add_button(
